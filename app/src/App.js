d49197e0ba9404de6d20318f0e42fa54c6252445
bill <manbil03@gmail.com>
2020-03-17 17:20:42 +1030

    added sign in

--- app/src/App.js
+++ app/src/App.js
@@ -19,26 +19,13 @@ ui.start('#firebaseui-auth-container', {
   ]
 });
 
-if (ui.isPendingRedirect()) {
-  ui.start('#firebaseui-auth-container', uiConfig);
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Keep It Tight</h1>
+        <div id="firebaseui-auth-container"></div>
+        <div id="loader">Loading...</div>
       </header>
     </div>
   );
